d40180385ddb97ea73eb1c57db4eb57d0d8c2d9f
Eduardo Gomez <edu21061991@gmail.com>
2023-01-26 13:05:03 +0100

    Prueba para el despliegue

--- src/main/java/dev/egomezfe/climby/mapper/ProvinceMapper.java
+++ src/main/java/dev/egomezfe/climby/mapper/ProvinceMapper.java
@@ -3,9 +3,11 @@ package dev.egomezfe.climby.mapper;
 
 import dev.egomezfe.climby.domain.Province;
 import dev.egomezfe.climby.dto.ProvinceDTO;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public interface ProvinceMapper {
 	
 	Province provinceDTOToprovince(ProvinceDTO provinceDTO);
