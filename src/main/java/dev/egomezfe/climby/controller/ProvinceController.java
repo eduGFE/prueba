561a33ca815fc4b07591fe00a1fe2a06bd3d7dc2
Eduardo Gomez <edu21061991@gmail.com>
2023-01-26 17:55:43 +0100

    Prueba para el despliegue

--- src/main/java/dev/egomezfe/climby/controller/ProvinceController.java
+++ src/main/java/dev/egomezfe/climby/controller/ProvinceController.java
@@ -1,7 +1,12 @@
 package dev.egomezfe.climby.controller;
 
 
+import dev.egomezfe.climby.domain.Province;
 import dev.egomezfe.climby.dto.ProvinceDTO;
+import dev.egomezfe.climby.model.Post;
+import dev.egomezfe.climby.repository.AuthorRepository;
+import dev.egomezfe.climby.repository.PostRepository;
+import dev.egomezfe.climby.repository.ProvinceRepository;
 import dev.egomezfe.climby.service.ProvinceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,12 +22,19 @@ import java.util.List;
 @RequestMapping("/province")
 public class ProvinceController {
 
+	@Autowired private ProvinceRepository provinceRepository;
 	@Autowired private ProvinceService provinceService;
 	
 	@ResponseStatus(HttpStatus.OK)
 	@GetMapping(value = "/provinces", headers = "Accept=application/json")
 	public List<ProvinceDTO> getProvinceFindAll(){
 		return provinceService.getProvinceFindAll();
+	}
+
+	@ResponseStatus(HttpStatus.OK)
+	@GetMapping(value = "/prueba", headers = "Accept=application/json")
+	public Iterable<Province> findAll() {
+		return provinceRepository.findAll();
 	}
 		
 	
